1d4c988e7771f82574df22a8306ced13e29ccc45
flexphere <ankh@flexphere.com>
2024-07-13 01:53:29 +0900

    fix

--- lua/mdrun/init.lua
+++ lua/mdrun/init.lua
@@ -83,7 +83,7 @@ M.run = function()
   local content = vim.treesitter.get_node_text(code_content, bufnr)
   for _, v in ipairs(config.cmds[lang]) do
     local replaced = v:gsub("{CODE_BLOCK}", content)
-    replaced = v:gsub("\\", "\\\\")
+    replaced = replaced:gsub("\\", "\\\\")
     table.insert(cmd, replaced)
   end
   table.insert(cmd, content)
